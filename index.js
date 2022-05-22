09653b0ee412edab389850fb9a2a9003563d7a84
Griffin Dooley <dooleygriffin@gmail.com>
2022-05-22 16:41:30 -0400

    commit tree 2661651d57c981e069f303489f620357279c731a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft () {document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+
+      if (left > 0) {
+      dodger.style.left = `${left - 9}px`;}
+    }
+  })}
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+      const rightNumbers = dodger.style.right.replace("px", "");
+      const right = parseInt(rightNumbers, 10);
+
+      if (right > 360) {
+      dodger.style.right = `${left + 9}px`;}
+    }
+  });
